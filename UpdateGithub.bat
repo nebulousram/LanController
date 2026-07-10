566389cbb43f675e874af437b91686745844de72
nebulousram <nebulousram@yahoo.com>
2026-07-10 11:49:08 -0500

    Update project with latest working changes

--- UpdateGithub.bat
+++ UpdateGithub.bat
@@ -1,6 +1,6 @@
 Any unnecessary lines I was suspcious of using git push origin main and git push -u origin master but I don't know shell script
 git init
-git remote add origin https://github.com/nebulousram/ScanController.git
+git remote add origin https://github.com/nebulousram/LanController.git
 git add .
 git commit -m "Update project with latest working changes"
 git push -u origin master
